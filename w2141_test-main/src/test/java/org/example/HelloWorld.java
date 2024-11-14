60578148a7977628894394635d352f60a5f60c1b
Xs <bagneverstop@gmail.com>
2024-11-14 10:45:28 +0200

    first test

--- w2141_test-main/src/test/java/org/example/HelloWorld.java
+++ w2141_test-main/src/test/java/org/example/HelloWorld.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "D:\\anatolie\\webdriver\\130.0.6723.58\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Xs\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
